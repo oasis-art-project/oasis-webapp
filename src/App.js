dfdd38ce55e727a1ce0c0a1c85bf3eb08f6cfbfb
carcruz <me@carcruzcast.com>
2019-07-19 17:02:37 -0600

    feat: config react-intl in project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { ThemeProvider } from "styled-components";
-import store from './store';
+import { addLocaleData, IntlProvider } from "react-intl";
+import en from "react-intl/locale-data/en";
+import es from "react-intl/locale-data/es";
+
+import store from "./store";
 import Root from "./containers/Root";
 
+import texts from './layout/texts.json';
 import theme from "./layout/theme";
+
+addLocaleData([...es, ...en]);
+
+const locale = navigator.languages.indexOf("es") >= 0 ? "es" : "en";
 
 const App = () => (
   <ReduxProvider store={store}>
     <ThemeProvider theme={theme}>
-      <Root />
+      <IntlProvider locale={locale} messages={texts[locale]}>
+        <Root />
+      </IntlProvider>
     </ThemeProvider>
   </ReduxProvider>
 );
