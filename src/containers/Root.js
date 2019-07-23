71d7e2bc385e3ab8d95fc90e998b62c1898b2f25
carcruz <me@carcruzcast.com>
2019-07-22 23:44:12 -0600

    style: remove focus outline

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Router, Route } from "react-router-dom";
 import history from "../helpers/history";
-import styled from 'styled-components';
+import styled from "styled-components";
 // pages
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 
 const MainLayout = styled.div`
-  font-family: 'Source Sans Pro', sans-serif;
-`
+  font-family: "Source Sans Pro", sans-serif;
+  *:focus {
+    outline: none !important;
+  }
+  .bp3-control input:focus ~ .bp3-control-indicator {
+    outline: none !important;
+  }
+`;
 
 const Root = () => (
   <Router history={history}>
