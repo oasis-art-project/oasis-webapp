0b0af2b01c15387387c9a7d1ca3fc0ebe4cc19fe
carcruz <me@carcruzcast.com>
2019-07-27 20:52:08 -0600

    feat: create api call to server

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-import { Tab, Tabs } from "../components";
+import { Tab, Tabs, Card, Loader } from "../components";
 import styled from "styled-components";
+
+import {get} from 'axios';
 
 const TabsContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
 
-const CurrentEvents = () => <div>Current envets</div>;
+
+class CurrentEvents extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {loading: true, events: null}
+  }
+
+  componentDidMount() {
+    get('api/event').then(response => {
+      console.log(response.data.events)
+    })
+    .catch( error => {
+      // handle error
+      console.log(error);
+    })
+  }
+
+  render() {
+    return <h1>hola !</h1>
+  }
+}
+
 const UpcomingEvents = () => <div>Upcoming events</div>;
 
 class HomeContainer extends Component {
