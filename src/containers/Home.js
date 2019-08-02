5c5c636d4f104fd32e0bf1f7d1e94e183a5a51bd
carcruz <me@carcruzcast.com>
2019-08-01 20:06:45 -0600

    feat: update home to do request to server

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,33 +2,44 @@ import React, { Component } from "react";
 import { Tab, Tabs, Card, Loader } from "../components";
 import styled from "styled-components";
 
-import {get} from 'axios';
+import { get } from "axios";
 
 const TabsContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
 
+const LoaderContainer = styled.div`
+  margin-top: 180px;
+`;
+const LoadingState = () => (
+  <LoaderContainer>
+    <Loader />
+  </LoaderContainer>
+);
 
 class CurrentEvents extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {loading: true, events: null}
+    this.state = { loading: true, events: null, error: false };
   }
 
   componentDidMount() {
-    get('api/event').then(response => {
-      console.log(response.data.events)
-    })
-    .catch( error => {
-      // handle error
-      console.log(error);
-    })
+    get("api/event")
+      .then(response => {
+        this.setState({ events: response.data.events, loading: false });
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
   }
 
   render() {
-    return <h1>hola !</h1>
+    if (this.state.loading) return <LoadingState />;
+    console.log(this.state.events);
+    return this.state.events.map(event => (
+      <Card key={event.id} title={event.name} description={event.description} />
+    ));
   }
 }
 
