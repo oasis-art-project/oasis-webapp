71b23473ef55586e09930bf33684a6edfadf2bfa
carcruz <me@carcruzcast.com>
2019-09-15 23:56:49 +0100

    feat: connect state to events view

--- src/components/event/index.js
+++ src/components/event/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Loader, Seo } from '../shared';
 
-const Event = ({ match }) => {
-  return (
-    <div>
-      <h3>ID: {match.params.id}</h3>
-    </div>
-  );
+const filterArray = (array, id) =>
+  array.filter(el => el.id === parseInt(id))[0];
+
+const Event = ({ match, events }) => {
+  if (events) {
+    const currentEvent = filterArray(events, match.params.id);
+    return (
+      <div>
+        <Seo title={currentEvent.name} />
+        <h3>ID: {match.params.id}</h3>
+      </div>
+    );
+  }
+  return <Loader />;
 };
 
 export default Event;
--- src/containers/Event.js
+++ src/containers/Event.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Event from '../components/event/index';
 
 const mapStateToProps = state => ({
-  events: state.event,
+  events: state.event.all,
 });
 
 const mapDispatchToProps = dispatch => ({});
