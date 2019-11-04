d57186da4175a823953d5dccdd98c63e6dc2361f
carcruz <me@carcruzcast.com>
2019-11-03 23:49:24 -0600

    refactor: refactor main Router and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { addLocaleData, IntlProvider } from 'react-intl';
 import throttle from 'lodash/throttle';
 import en from 'react-intl/locale-data/en';
 import es from 'react-intl/locale-data/es';
-
 import createStore from './store';
 import { saveState } from './localStorage';
-import Root from './containers/Root';
-
 import texts from './layout/texts.json';
 import theme from './layout/theme';
+import history from './helpers/history';
+import Layout from './layout/MainLayout';
+// Main Pages //
+import IndexView from './containers/IndexView';
+import Login from './containers/Login';
 
 addLocaleData([...es, ...en]);
-
 const locale = navigator.languages.indexOf('es') >= 0 ? 'es' : 'en';
-
 const store = createStore();
 
+// Save user and auth when store change
+// may be too much so used throttle from lodash
 store.subscribe(
   throttle(() => {
     saveState({
@@ -36,7 +39,12 @@ const App = () => (
   <ReduxProvider store={store}>
     <ThemeProvider theme={theme}>
       <IntlProvider locale={locale} messages={texts['en']}>
-        <Root />
+        <Router history={history}>
+          <Layout>
+            <Route path="/login" component={Login} />
+            <Route path="/" component={IndexView} />
+          </Layout>
+        </Router>
       </IntlProvider>
     </ThemeProvider>
   </ReduxProvider>
--- src/layout/theme.js
+++ src/layout/theme.js
@@ -1,13 +1,14 @@
 // This is the gloal theme
 
 const theme = {
-  text_color: "#425663",
+  textBaseFont: "'Source Sans Pro', sans-serif;'",
+  text_color: '#425663',
   colors: {
-    green: "#B0CFD1",
-    white: "#fff",
-    orange: "#F48438",
-    grey: "#425663"
-  }
+    green: '#B0CFD1',
+    white: '#fff',
+    orange: '#F48438',
+    grey: '#425663',
+  },
 };
 
 export default theme;
