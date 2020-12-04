62b386f1149f0ce74aaa4f9da280a3fef3fc0691
codeanticode <andres.colubri@gmail.com>
2020-12-04 10:21:23 -0500

    removed command line arg

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,11 +1,6 @@
 import { isProd } from '../helpers/index';
 
-const connectTo = process.argv.connectTo || 'local';
-
 const devAPI = '/api';
 const prodAPI = 'https://server-oasis.herokuapp.com/api';
 
-const devSetUp = connectTo === 'prod' ? prodAPI : devAPI;
-
-export const API_URL = isProd ? prodAPI : devSetUp;
-// export const API_URL = prodAPI;
+export const API_URL = isProd ? prodAPI : devAPI;
