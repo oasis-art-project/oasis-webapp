663a9a9188acfa8490db2292b9eab4ede11b903b
codeanticode <andres.colubri@gmail.com>
2020-12-06 16:32:33 -0500

    removed unused const in artwork section

--- src/pages/artist/ArtworkSection.js
+++ src/pages/artist/ArtworkSection.js
@@ -1,39 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Card } from '../../components';
 import { IMGS_URL } from '../../helpers/index';
-
-const Container = styled.div`
-  display: flex;
-  margin-bottom: 30px;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  vertical-align: middle;
-`;
-
-const ImageContainer = styled.div`
-  width: 100px;
-  height: 100px;
-  margin-right: 20px;
-  display: table-cell;
-  text-align: center;
-  vertical-align: middle;
-  overflow: hidden;
-`;
-
-const Name = styled.h3`
-  font-weight: 400;
-  margin: 0;
-`;
-
-const PropArtwork = styled.p`
-  font-weight: 400;
-  margin: 0;
-`;
-
-const PropsContainer = styled.div``;
 
 const ArtworkSection = ({ artwork }) => (
     <Card
@@ -47,5 +14,5 @@ const ArtworkSection = ({ artwork }) => (
       kind="artwork"
     />
 );
-  
+
 export default ArtworkSection;
