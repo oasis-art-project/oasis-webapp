65c7a30077b8c6259f3622c93742c10e23cf3a2c
carcruz <me@carcruzcast.com>
2021-03-02 15:51:06 -0600

    fix: sorting on artis

--- src/pages/Artists/Artists.tsx
+++ src/pages/Artists/Artists.tsx
@@ -1,26 +1,35 @@
 import { Link } from 'react-router-dom';
 import useArtists from '../../hooks/useArtists';
 import { IMGS_URL } from '../../helpers';
+import styled from 'styled-components';
 import Loader from '../../components/Loader';
 
 const formatName = (first: string, last: string) => {
-  return (last + ' ' + first).trim()
+  return (last + ' ' + first).trim();
 };
+
+const Tags = styled.p`
+  min-width: 100%;
+  min-height: 1em;
+`;
 
 function Artists() {
   const { status, data, error } = useArtists();
 
-  if (data) {
-    data.users.sort((a: any, b: any) => {
-      return formatName(a.firstName, a.lastName) > formatName(b.firstName, b.lastName);
-    });
-  }
-
   if (status === 'loading') return <Loader />;
   if (error) return <div>Error</div>;
+
+  const sortedData = data.users.sort((a: any, b: any) => {
+    const nameA = formatName(a.firstName, a.lastName);
+    const nameB = formatName(b.firstName, b.lastName);
+    if (nameA > nameB) return 1;
+    if (nameA < nameB) return -1;
+    return 0;
+  });
+
   return (
     <div className="grid xl:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 gap-6 mb-5">
-      {data.users.map((artist: any) => (
+      {sortedData.map((artist: any) => (
         <Link key={artist.id} to={`/artist/${artist.id}`}>
           <article className="flex flex-end flex-col h-full justify-end">
             <p className="font-header font-bold text-xl md:truncate mb-1 uppercase">
@@ -31,7 +40,9 @@ function Artists() {
               src={`${IMGS_URL}/${artist.prevImages[0]}`}
               alt={(artist.firstName + ' ' + artist.lastName).trim()}
             />
-            <p className="md:truncate mb-1">{artist.tags.split(';').map((tag: any) => tag + " ")}</p>
+            <Tags className="md:truncate mb-1 leading-4">
+              {artist.tags.split(';').map((tag: any) => tag + ' ')}
+            </Tags>
           </article>
         </Link>
       ))}
