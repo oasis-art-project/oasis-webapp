e9a4a84a4edcdc69be2b84314d2c0fb91c7c072a
codeanticode <andres.colubri@gmail.com>
2021-03-02 20:24:38 -0500

    added info dialog before opening hubs from event page

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -134,7 +134,6 @@ function Event() {
       {data.event.hubs_link && (
         <HubsButton
           className="flex justify-center gap-5 w-full"
-          // href={`https://hubs.link/${data.event.hubs_link}`}
           onClick={openHubsDialog}
         >
           <img src={cubeImage} alt="3D Cube" width="50" />
@@ -196,22 +195,34 @@ function Event() {
           </Link>
         ))}
       </div>
+
       <StyledDialog isOpen={showDialogHubs} onDismiss={closeHubsDialog} aria-label="Hubs intro">
         <button className="close-button float-rigt" onClick={closeHubsDialog}>
           <span aria-hidden>×</span>
         </button>
         <div className="relative text-center">
           <p className="mt-6 mb-12 text-xl font-header">
-            Only logged-in users can chat with other users
+            OASIS will now take you to the virtual event. Some important instructions:
           </p>
-          <Link
-            className="mx-auto border-solid border-4 border-darkGray px-3 py-1 font-header font-bold text-xl"
-            to="/login"
+          <p className="mt-6 mb-12 text-xl font-header">
+            <b>1)</b> Select <b>JOIN ROOM</b> in the next page. Enter on Device only if you have a VR headset.
+          </p>
+          <p className="mt-6 mb-12 text-xl font-header">
+          <b>2)</b> After joining, use the <b>W KEY</b> to move forward, <b>S KEY</b> to move backwards, and <b>mouse pointer</b> to set direction.
+          </p>
+          <p className="mt-6 mb-12 text-xl font-header">
+          <b>3)</b> If you are using a phone, <b>PINCH IN/OUT</b> to move foward/backwards, and <b>move the phone</b> around to set direction.
+          </p>          
+          <a className="mx-auto border-solid border-4 border-darkGray px-3 py-1 font-header font-bold text-xl"
+             target="_blank"
+             rel="noreferrer"                        
+             href={`https://hubs.link/${data.event.hubs_link}`}
           >
-            Login
-          </Link>
+            Continue
+          </a>
         </div>
       </StyledDialog>
+
     </div>
   );
 }
