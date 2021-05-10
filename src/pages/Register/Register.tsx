76223efc2eeae9bb9efe382b4c5adb3cd81a3d2c
codeanticode <andres.colubri@gmail.com>
2021-05-10 14:13:10 -0400

    limit checks

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -9,15 +9,15 @@ import { registerInfoQuery } from '../../hooks/useRegister';
 import logo from '../../assets/img/logo-v2.png';
 
 const registerSchema = Yup.object().shape({
-  firstName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
-  lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Required'),
+  firstName: Yup.string().min(2, 'Too Short, enter at least two characters').max(50, 'Too long, maximum 50 characters').required('Required'),
+  lastName: Yup.string().max(50, 'Too long, maximum 50 characters'),
   email: Yup.string().email('Invalid email').required('Required'),
-  phone: Yup.string(),
+  phone: Yup.string().max(10, 'Too long, maximum 10 characters'),
   role: Yup.string().required('Required'),
-  website: Yup.string(),
-  instagram: Yup.string(),
-  youtube: Yup.string(),
-  bio: Yup.string(),
+  website: Yup.string().max(100, 'Too long, maximum 100 characters'),
+  instagram: Yup.string().max(30, 'Too long, maximum 30 characters'),
+  youtube: Yup.string().max(30, 'Too long, maximum 30 characters'),
+  bio: Yup.string().max(2000, 'Too long, maximum 2000 characters')
 });
 
 const getRole = (role: String) => {
