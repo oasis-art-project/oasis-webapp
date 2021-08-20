ed5474790774ea5d588d7068371eb49e4b7a772d
codeanticode <andres.colubri@gmail.com>
2021-08-20 15:22:05 -0400

    artwork fields

--- src/pages/AddArtwork/ArtworkInfo.tsx
+++ src/pages/AddArtwork/ArtworkInfo.tsx
@@ -2,18 +2,13 @@ import * as Yup from 'yup';
 import { useFormik } from 'formik';
 import SectionHeader from '../../components/SectionHeader';
 
-const profileSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short, enter at least two characters')
-    .max(50, 'Too long, maximum 50 characters')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-  lastName: Yup.string().max(50, 'Too long, maximum 50 characters'),
-  phone: Yup.string().max(10, 'Too long, maximum 10 characters'),
-  homepage: Yup.string().max(100, 'Too long, maximum 100 characters'),
-  instagram: Yup.string().max(30, 'Too long, maximum 30 characters'),
-  youtube: Yup.string().max(30, 'Too long, maximum 30 characters'),
-  bio: Yup.string().max(2000, 'Too long, maximum 2000 characters'),
+const artworkSchema = Yup.object().shape({
+  name: Yup.string().max(100, 'Too long, maximum 100 characters'),
+  description: Yup.string().max(1000, 'Too long, maximum 10000 characters'),
+  medium: Yup.string().max(200, 'Too long, maximum 10000 characters'),
+  size: Yup.string().max(200, 'Too long, maximum 10000 characters'),
+  year: Yup.number().integer('invalid decimal'),
+  link: Yup.string().max(100, 'Too long, maximum 100 characters')
 });
 
 function ArtworkInfo(props: any) {
@@ -21,16 +16,14 @@ function ArtworkInfo(props: any) {
 
   const formik = useFormik({
     initialValues: {
-      firstName: user.firstName || '',
-      lastName: user.lastName || '',
-      email: email || '',
-      phone: user.phone || '',
-      bio: user.bio || '',
-      homepage: user.homepage || '',
-      instagram: user.instagram || '',
-      youtube: user.youtube || '',
+      name: user.name || '',
+      description: user.description || '',
+      medium: user.medium || '',
+      size: user.size || '',
+      year: user.year || '',
+      link: user.link || '',
     },
-    validationSchema: profileSchema,
+    validationSchema: artworkSchema,
     onSubmit: (values: any) => {
       const update = { ...values, confirmed: true };
       mutation(update);
@@ -45,100 +38,99 @@ function ArtworkInfo(props: any) {
             <SectionHeader title="User information" />
             <div className="grid grid-cols-10 gap-6">
               <div className="col-span-10 sm:col-span-5">
-                <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
-                  First name
+                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                  Work name
                 </label>
                 <input
                   type="text"
-                  name="firstName"
-                  id="firstName"
-                  autoComplete="given-name"
+                  name="name"
+                  id="name"
+                  autoComplete="work-title"
                   className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
                   onChange={formik.handleChange}
-                  value={formik.values.firstName}
+                  value={formik.values.name}
                 />
-                {formik.errors.firstName && (
-                  <p className="mt-2 text-sm text-red-500">{formik.errors.firstName}</p>
-                )}
-              </div>
-
-              <div className="col-span-10 sm:col-span-5">
-                <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
-                  Last name
-                </label>
-                <input
-                  type="text"
-                  name="lastName"
-                  id="lastName"
-                  autoComplete="family-name"
-                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
-                  onChange={formik.handleChange}
-                  value={formik.values.lastName}
-                />
-                {formik.errors.lastName && (
-                  <p className="mt-2 text-sm text-red-500">{formik.errors.lastName}</p>
-                )}
-              </div>
-
-              <div className="col-span-10 sm:col-span-7">
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Email address
-                </label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  autoComplete="email"
-                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                />
-                {formik.errors.email && (
-                  <p className="mt-2 text-sm text-red-500">{formik.errors.email}</p>
-                )}
-              </div>
-
-              <div className="col-span-10 sm:col-span-7">
-                <label htmlFor="phone" className="block text-sm font-medium text-gray-700">
-                  Phone number (only used to send direct message notifications)
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  autoComplete="phone"
-                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
-                  onChange={formik.handleChange}
-                  value={formik.values.phone}
-                />
-                {formik.errors.phone && (
-                  <p className="mt-2 text-sm text-red-500">{formik.errors.phone}</p>
+                {formik.errors.name && (
+                  <p className="mt-2 text-sm text-red-500">{formik.errors.name}</p>
                 )}
               </div>
 
               <div className="col-span-10">
                 <label htmlFor="bio" className="block text-sm font-medium text-gray-700">
-                  Short bio
+                  Description
                 </label>
                 <div className="mt-1">
                   <textarea
-                    id="bio"
-                    name="bio"
+                    id="description"
+                    name="description"
                     rows={3}
                     className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300"
                     placeholder=""
                     onChange={formik.handleChange}
-                    value={formik.values.bio}
+                    value={formik.values.description}
                   />
-                  {formik.errors.bio && (
-                    <p className="mt-2 text-sm text-red-500">{formik.errors.bio}</p>
+                  {formik.errors.description && (
+                    <p className="mt-2 text-sm text-red-500">{formik.errors.description}</p>
                   )}
                 </div>
               </div>
 
+
+              <div className="col-span-10  sm:col-span-5">
+                <label htmlFor="instagram" className="block text-sm font-medium text-gray-700">
+                  Medium
+                </label>
+                <input
+                  type="text"
+                  name="Medium"
+                  id="Medium"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
+                  onChange={formik.handleChange}
+                  value={formik.values.medium}
+                />
+                {formik.errors.medium && (
+                  <p className="mt-2 text-sm text-red-500">{formik.errors.medium}</p>
+                )}
+              </div>
+
+
+              <div className="col-span-10  sm:col-span-5">
+                <label htmlFor="instagram" className="block text-sm font-medium text-gray-700">
+                  Size
+                </label>
+                <input
+                  type="text"
+                  name="Size"
+                  id="Size"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
+                  onChange={formik.handleChange}
+                  value={formik.values.size}
+                />
+                {formik.errors.size && (
+                  <p className="mt-2 text-sm text-red-500">{formik.errors.size}</p>
+                )}
+              </div>
+
+              <div className="col-span-10  sm:col-span-5">
+                <label htmlFor="instagram" className="block text-sm font-medium text-gray-700">
+                  Year
+                </label>
+                <input
+                  type="integer"
+                  name="Year"
+                  id="Year"
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
+                  onChange={formik.handleChange}
+                  value={formik.values.year}
+                />
+                {formik.errors.size && (
+                  <p className="mt-2 text-sm text-red-500">{formik.errors.size}</p>
+                )}
+              </div>
+
               <div className="col-span-10  sm:col-span-7">
                 <label htmlFor="homepage" className="block text-sm font-medium text-gray-700">
-                  Website
+                  Link
                 </label>
                 <div className="mt-1 flex shadow-sm">
                   <span className="inline-flex items-center px-3 border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
@@ -146,52 +138,19 @@ function ArtworkInfo(props: any) {
                   </span>
                   <input
                     type="text"
-                    name="homepage"
-                    id="homepage"
+                    name="link"
+                    id="link"
                     className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full sm:text-sm border-gray-300"
                     placeholder=""
                     onChange={formik.handleChange}
-                    value={formik.values.homepage}
+                    value={formik.values.link}
                   />
-                  {formik.errors.homepage && (
-                    <p className="mt-2 text-sm text-red-500">{formik.errors.homepage}</p>
+                  {formik.errors.link && (
+                    <p className="mt-2 text-sm text-red-500">{formik.errors.link}</p>
                   )}
                 </div>
               </div>
 
-              <div className="col-span-10  sm:col-span-5">
-                <label htmlFor="instagram" className="block text-sm font-medium text-gray-700">
-                  Instagram
-                </label>
-                <input
-                  type="text"
-                  name="instagram"
-                  id="instagram"
-                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
-                  onChange={formik.handleChange}
-                  value={formik.values.instagram}
-                />
-                {formik.errors.instagram && (
-                  <p className="mt-2 text-sm text-red-500">{formik.errors.instagram}</p>
-                )}
-              </div>
-
-              <div className="col-span-10  sm:col-span-5">
-                <label htmlFor="youtube" className="block text-sm font-medium text-gray-700">
-                  YouTube
-                </label>
-                <input
-                  type="text"
-                  name="youtube"
-                  id="youtube"
-                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300"
-                  onChange={formik.handleChange}
-                  value={formik.values.youtube}
-                />
-                {formik.errors.youtube && (
-                  <p className="mt-2 text-sm text-red-500">{formik.errors.youtube}</p>
-                )}
-              </div>
             </div>
           </div>
           <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
